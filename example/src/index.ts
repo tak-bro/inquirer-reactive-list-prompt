7db4a588814967460b3a08bb3cf5669548fbfea8
Tak <env.tak@gmail.com>
2023-12-10 17:42:52 +0900

    Feature/setup inquirer (#2)
    * Feature/setup prettier (#1)
    
    * feat: configure eslint
    
    * feat: configure prettier and husky
    
    * chore: temp commit
    
    * feat: set MutableListPrompt type
    
    * chore: temp commit
    
    * feat: setup MutableListPrompt
    
    * feat: update choices$
    
    * feat: add loader concept
    
    * chore: refactor project name
    
    * chore: temp commit
    
    * feat: add doneFrame for loader
    
    * feat: update ReactiveListLoader type
    
    * feat: update workflows and update package name

---
+++ example/src/index.ts
@@ -0,0 +1,62 @@
+import inquirer from 'inquirer';
+import ReactiveListPrompt, { ReactiveListChoice, ReactiveListLoader } from 'inquirer-reactive-list-prompt';
+import { BehaviorSubject } from 'rxjs';
+
+const choices$: BehaviorSubject<ReactiveListChoice[]> = new BehaviorSubject<ReactiveListChoice[]>([]);
+const loader$: BehaviorSubject<ReactiveListLoader> = new BehaviorSubject<ReactiveListLoader>({
+    isLoading: false,
+    message: 'AI is analyzing...',
+    // startOption: {
+    //     color: 'red',
+    //     spinner: 'triangle'
+    // },
+    // stopOption: {
+    //     doneFrame: '✖',
+    //     color: 'red'
+    // }
+});
+
+inquirer.registerPrompt('reactiveListPrompt', ReactiveListPrompt);
+const mutableList = inquirer.prompt({
+    type: 'reactiveListPrompt',
+    name: 'test',
+    message: 'Select AI message',
+    emptyMessage: 'Nothing to show',
+    choices$,
+    loader$,
+});
+
+mutableList.then((answer: any) => {
+    choices$.complete();
+    loader$.complete();
+    console.log('answer: ', answer);
+});
+
+setTimeout(() => {
+    loader$.next({ isLoading: true });
+    choices$.next([
+        { name: 'test1', value: 'test1' },
+        new inquirer.Separator(),
+        { name: 'test2', value: 'test2' },
+        { name: 'test3', value: 'test3', disabled: true },
+    ]);
+}, 2000);
+
+setTimeout(() => {
+    choices$.next([
+        { name: 'test4', value: 'test4' },
+        { name: 'test5', value: 'test5', disabled: true },
+        { name: 'test6', value: 'test6', disabled: true, isError: true },
+    ]);
+}, 5000);
+
+setTimeout(() => {
+    loader$.next({
+        isLoading: false,
+        message: 'AI is analyzed',
+        // stopOption: {
+        //     doneFrame: '⚠', // '✖'
+        //     color: 'yellow' // 'red'
+        // }
+    });
+}, 6000);
